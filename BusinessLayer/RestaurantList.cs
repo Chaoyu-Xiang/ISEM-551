ffaacea23fabc28dc2dccf1d5d7422f216ed3cd5
Wenwen Zhang <Wenwen.Zhang@intlfcstone.com>
2019-07-11 20:15:27 -0500

    project2

---
+++ BusinessLayer/RestaurantList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using DataLayer;
+using System.Data;
+
+
+namespace BusinessLayer
+{
+    public class RestaurantList
+    {
+        public static List<RestaurantInformation> GetRestaurantDate()
+        {
+            RestaurantInformation dataEntry = new RestaurantInformation();
+            var dataEntryList = new List<RestaurantInformation>();
+
+            DataSet ds = new DataSet();
+
+            ds = RestaurantDbAccess.GetDataUsingDB();
+
+            if (ds.Tables.Count > 0)
+            {
+                dataEntryList = ds.Tables[0].AsEnumerable().Select(m => new RestaurantInformation
+                {
+                    Name = Convert.ToString(m["Name"]),
+                    Country = Convert.ToString(m["Country"]),
+                    City = Convert.ToString(m["City"]),
+                    AwardedSince = Convert.ToString(m["AwardedSince"])
+                }).ToList();
+            }
+
+            return dataEntryList;
+
+        }
+    }
+
+       
+}
---
+++ Project1/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer;
+
+namespace ISEM551Project1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult ISEM551()
+        {
+            ViewBag.Message = "Our Team Member";
+
+            return View();
+        }
+
+
+        public ActionResult DisplayData()
+        {
+            ViewBag.Message = "Data";
+
+            var data = RestaurantList.GetRestaurantDate();
+
+            return View(data);
+        }
+    }
+}
