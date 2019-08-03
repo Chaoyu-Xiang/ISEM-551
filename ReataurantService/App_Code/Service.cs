bbdf2d93eff7e3ded4452df3fede4ead2c482e8f
yzhou7132 <fancy3361@gmail.com>
2019-08-02 22:03:30 -0400

    hw3

---
+++ ReataurantService/App_Code/Service.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Models;
+using BusinessLayer;
+
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class Service : IService
+    {
+        public List<Restaurant> GetRestaurantsList()
+        {
+            List<Restaurant> returnList = new List<Restaurant>();
+            List<RestaurantInformation> dataList = RestaurantList.GetRestaurantData();
+            foreach (RestaurantInformation item in dataList)
+            {
+                Restaurant rest = new Restaurant(); 
+                rest.Name = item.Name;
+                rest.Country = item.Country;
+                rest.City = item.City;
+                rest.AwardedSince = item.AwardedSince;
+                returnList.Add(rest);
+            }
+            return returnList;
+        }
+    }
